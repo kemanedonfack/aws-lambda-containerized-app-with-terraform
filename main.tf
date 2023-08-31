c11b643a915b8f3729a6ab609639f624eb8a13f3
Kemane Donfack <kemanedonfack5@gmail.com>
2023-08-31 11:47:47 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,41 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "iam_for_lambda"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [{
+      "Action" : "sts:AssumeRole",
+      "Principal" : {
+        "Service" : "lambda.amazonaws.com"
+      },
+      "Effect" : "Allow"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+  name = "aws_iam_policy_lambda_role"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "*"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+resource "aws_lambda_function" "python-lambda-function" {
+  function_name = "python-lambda-function"
+  description   = "lambda function from terraform"
+  image_uri     = "lugar2020/spring-backend"
+  package_type  = "Image"
+  architectures = ["x86_64"]
+  role          = aws_iam_role.lambda_role.arn
+}
